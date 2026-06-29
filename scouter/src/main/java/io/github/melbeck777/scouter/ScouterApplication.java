2eb26a2a402ca3fd06e25a09ef5a1f988cbbbe18
Melbeck777 <molmin729@gmail.com>
2026-06-29 14:48:38 +0900

    add: made by initializr

---
+++ scouter/src/main/java/io/github/melbeck777/scouter/ScouterApplication.java
@@ -0,0 +1,13 @@
+package io.github.melbeck777.scouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScouterApplication.class, args);
+	}
+
+}
